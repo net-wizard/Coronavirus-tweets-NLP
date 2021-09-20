4397c73b1b68ef9ab8256a04494590b05b4257d2
Shubham Thorat <shubhamthorat2202@gmail.com>
2021-09-20 12:07:25 +0530

    text_input to text_area

--- App/frontend/front.py
+++ App/frontend/front.py
@@ -6,7 +6,7 @@ st.title("Corona tweet sentiment analysis app")
 
 st.write("Takes tweet about corona virus cleans it analyse it and gives output positive, negative or nutral as a output")
 
-tweet=st.text_input("Paste a tweet")
+tweet=st.text_area("Paste a tweet")
 
 if st.button("Predict"):
     if tweet is not None:
